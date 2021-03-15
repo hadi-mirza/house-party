e04777ff54def80e9845bc79a7e602bef9ccdad6
hadi-mirza <hadimirza1@gmail.com>
2021-03-15 19:39:29 -0400

    add routes for creating room and room id lookup if exists

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import RoomJoinPage from '../RoomJoinPage/RoomJoinPage'
 import CreateRoomPage from '../CreateRoomPage/CreateRoomPage'
 import { BrowserRouter as Router, Switch, Route, Link, Redirect, } from "react-router-dom"
+import Room from '../Room/Room'
 
 export class Home extends Component {
     render() {
@@ -14,6 +15,7 @@ export class Home extends Component {
                             </Route>
                         <Route path="/join" component={RoomJoinPage} />
                         <Route path="/create" component={CreateRoomPage} />
+                        <Route path='/room/:roomCode' component={Room} />
                     </Switch>
                 </Router>
             </div>
--- houseparty/urls.py
+++ houseparty/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html')),
     path('join/', TemplateView.as_view(template_name='index.html')),
     path('create/', TemplateView.as_view(template_name='index.html')),
+    path('room/<str:roomCode>', TemplateView.as_view(template_name='index.html')),
 ]
