7c209f28698b3b5f694e3a63da73aa602837d515
hadi-mirza <hadimirza1@gmail.com>
2021-03-15 21:41:20 -0400

    add mount method if user session is already created on home page. redirect to room if it exists

--- api/views.py
+++ api/views.py
@@ -1,3 +1,4 @@
+from django.http.response import JsonResponse
 import rest_framework
 from api.serializers import RoomSerializer, CreateRoomSerializer
 from django.db.models import query
@@ -76,3 +77,13 @@ class CreateRoomView(APIView):
                 return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
         
         return Response({'Bad Request': 'Invalid data...'}, status=status.HTTP_400_BAD_REQUEST)
+
+class UserInRoom(APIView):
+    def get(self, reqest, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        data = {
+            'code': self.request.session.get('room_code')
+        }
+        return JsonResponse(data, status=status.HTTP_200_OK)
--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,18 +1,59 @@
 import React, { Component } from 'react'
 import RoomJoinPage from '../RoomJoinPage/RoomJoinPage'
 import CreateRoomPage from '../CreateRoomPage/CreateRoomPage'
+import { Grid, Button, ButtonGroup, Typography } from '@material-ui/core'
 import { BrowserRouter as Router, Switch, Route, Link, Redirect, } from "react-router-dom"
 import Room from '../Room/Room'
 
 export class Home extends Component {
+    constructor() {
+        super()
+        this.state = {
+            roomCode: null,
+        }
+
+    }
+    
+    async componentDidMount() {
+        fetch('/api/user-in-room')
+        .then((response) => response.json())
+        .then((data) => {
+            this.setState({
+                roomCode: data.code
+            })
+        })
+    }
+
+    renderHomePage() {
+        return (
+            <Grid container spacing={3}>
+                <Grid item xs={12} align="center">
+                    <Typography variant="h3" component="h3">
+                        House Party
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <ButtonGroup disableElevation variant="contained" color="primary">
+                        <Button color="primary" to="/join" component={ Link }>
+                            Join a Room
+                        </Button>
+                        <Button color="secondary" to="/create" component={ Link }>
+                            Create a Room
+                        </Button>
+                    </ButtonGroup>
+                    </Grid>
+            </Grid>
+        )
+    }
+
     render() {
         return (
             <div className="Home">
                 <Router>
                     <Switch>
-                        <Route exact path="/">
-                            <p>This is the home page</p>
-                            </Route>
+                        <Route exact path="/" render={() => {
+                            return this.state.roomCode ? (<Redirect to={`/room/${this.state.roomCode}`} />) : this.renderHomePage()
+                        }} />
                         <Route path="/join" component={RoomJoinPage} />
                         <Route path="/create" component={CreateRoomPage} />
                         <Route path='/room/:roomCode' component={Room} />
